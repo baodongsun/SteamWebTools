3c15c277ea451113d6b9e039d2dd0b5381ca2911
baodongsun <maccoy@qq.com>
2014-12-14 14:41:52 +0800

    Update steamwebtools.init.user.js

--- steamwebtools.init.user.js
+++ steamwebtools.init.user.js
@@ -15,5 +15,5 @@
 
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = '//maccoyhaha.github.io/SteamWebTools/steamwebtools.base.js';
+script.src = '//baodongsun.github.io/SteamWebTools/steamwebtools.base.js';
 document.body.appendChild(script);
